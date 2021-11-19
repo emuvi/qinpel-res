430b91497132bd84be8d66c67e66414899bfb70e
Éverton M. Vieira <everton.muvi@gmail.com>
2021-11-18 21:25:11 -0300

    prototype development

--- types/all.d.ts
+++
@@ -1,7 +0,0 @@
-export { QinDragCalls, QinEvent, QinAction, QinArm } from "./qin-arm";
-export { QinBody } from "./qin-body";
-export { QinFilesNature, QinFilesOperation, QinFilesDescriptor, QinFoot } from "./qin-foot";
-export { QinPoint, QinDimension, QinBounds, QinGrandeur, QinHead } from "./qin-head";
-export { QinStyles, QinSkin } from "./qin-skin";
-export { QinSoul } from "./qin-soul";
-//# sourceMappingURL=all.d.ts.map
